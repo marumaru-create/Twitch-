61b5118f7763b8011cca413d910f52988ab64ad4
marumaru-create <tmaru4171@gmail.com>
2026-01-11 18:13:20 +0900

    prettier,eslintの設定

---
+++ eslint.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs';
+import prettier from 'eslint-config-prettier';
+
+export default withNuxt(
+  // Your custom configs here
+  prettier
+);
